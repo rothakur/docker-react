df36b66df756864e2d2288312d0b97c1e2d7682d
Thakur <rothakur@informatica.com>
2020-02-11 15:11:52 +0530

    changed app text again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload it now.
+          Edit <code>src/App.js</code> and save to reload it now to check redeploy.
         </p>
         <a
           className="App-link"
